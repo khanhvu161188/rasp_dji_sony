d83aaf3e3367817eafda83d922ee49075d6b8412
Khanh Vu <khanh.vu@insightus.com.au>
2018-06-21 03:17:01 +1000

    subcribe redis

--- webserver/server.js
+++ webserver/server.js
@@ -3,6 +3,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var SonyCamera = require("../index.js");
 
+var redis = require("redis");
+var sub = redis.createClient();
+
 var cam = new SonyCamera();
 
 cam.on('update', function(param, value) {
@@ -57,6 +60,54 @@ io.on('connection', function(socket){
     });
 
 });
+
+
+sub.on("message", function (channel, msg) {
+    if(channel=="c_connect"){
+        console.log("connect camera here");
+        cam.connect();
+    }
+    else if(channel=="c_disconnect"){
+        console.log("disconnect");
+        cam.disconnect();
+    }
+    else if(channel=="c_zoomin"){
+        console.log("zoom in");
+        cam.zoomIn();
+    }
+    else if(channel=="c_zoomout"){
+        console.log("zoom out");
+        cam.zoomOut();
+    }
+    else if(channel=="c_capture"){
+        console.log("capture for id:" + msg);
+        cam.capture(true, function(err, name, image) {
+            if(err) {
+                //return io.emit("status", "Error: " + err);
+                console.log('error when capture');
+            }
+            if(image)
+            {
+                //io.emit('image', image.toString('base64'));
+
+            }
+            if(name && !image)
+            {
+                // io.emit('status', "new photo: " + name);
+                console.log(name);
+            }
+        });
+    }
+    else{
+    }
+
+});
+sub.subscribe("c_connect");
+sub.subscribe("c_disconnect");
+sub.subscribe("c_capture");
+sub.subscribe("c_zoomin");
+sub.subscribe("c_zoomout");
+
 
 http.listen(4000, function(){
   console.log('listening on *:4000');
